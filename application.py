2521edda1e719668386d1ae3492804fdb9b9d39e
joviyal-arun <joviyalarun07@gmail.com>
2024-09-20 12:46:22 +0530

    merging commit

--- application.py
+++ application.py
@@ -1,18 +1,22 @@
 def addition(a,b):
-    c=a+b+10
+    c=a+b
     return c
 
 def divya_addition(a,b):
-    c=a+b+10
-    return c
-
-def arun_addition(a,b):
     c=a+b
     return c
+
+def arun_addition(a, b):
+    c = a + b + 10  # Added 10 to the sum
+    return c
+
 output=arun_addition(12,23)
 print(output)
 
 def multiply_operation(a,b):
     c=a*b+10
     print(c)
+    return c
+def div_operation(a,b):
+    c=a%b
     return c
